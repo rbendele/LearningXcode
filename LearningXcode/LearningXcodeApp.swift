86b54c9d58bbbd395f96ca842b7cba46e3543edc
Ron Bendele <fscj@bendele.net>
2021-04-12 14:27:18 -0400

    Initial Commit

---
+++ LearningXcode/LearningXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningXcodeApp.swift
+//  LearningXcode
+//
+//  Created by Ron Bendele on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
